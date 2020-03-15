dd6d4f15aa57fd9256b0e4a0575230d76f1d09c9
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-03-15 00:02:23 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Hello
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
